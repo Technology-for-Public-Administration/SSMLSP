98e32ea501d3f1e7c192fab36e560329389118e5
Feily Zhang <fei472625@foxmail.com>
2019-07-17 09:23:56 +0800

    init repo

---
+++ src/main/java/tech/feily/Mystar2/MyStar2/main/App.java
@@ -0,0 +1,13 @@
+package tech.feily.Mystar2.MyStar2.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
